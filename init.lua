2a7183383158a6448fb989547b7330f96d8f64f4
ndys <hendy.sg02@gmail.com>
2025-03-25 23:25:11 +0700

    initial configuration

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- Load core configurations
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
+-- Load LazyVim setup
+require("config.lazy")
---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
