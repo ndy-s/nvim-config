de6b44015f6fe1e9d7c53ddd51160630b7376faa
ndys <hendy.sg02@gmail.com>
2025-03-30 15:24:28 +0700

    [fix] update plugins code

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,16 +5,16 @@
 return {
     -- If you want neo-tree's file operations to work with LSP (updating imports, etc.), you can use a plugin like
     -- https://github.com/antosha417/nvim-lsp-file-operations:
-    -- {
-    --   "antosha417/nvim-lsp-file-operations",
-    --   dependencies = {
-    --     "nvim-lua/plenary.nvim",
-    --     "nvim-neo-tree/neo-tree.nvim",
-    --   },
-    --   config = function()
-    --     require("lsp-file-operations").setup()
-    --   end,
-    -- },
+    {
+      "antosha417/nvim-lsp-file-operations",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-neo-tree/neo-tree.nvim",
+      },
+      config = function()
+        require("lsp-file-operations").setup()
+      end,
+    },
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,28 +1,34 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    keys = {
-        -- {
-        --   "<leader>?",
-        --   function()
-        --     require("which-key").show({ global = false })
-        --   end,
-        --   desc = "Buffer Local Keymaps (which-key)",
-        -- },
-    },
-    config = function()
-        local wk = require("which-key")
-        wk.add({
-            { "<leader>c", group = "Code (Diagnostics)"},
-            { "<leader>d", group = "Debugger (DAP)" },
-            { "<leader>g", group = "Code Navigation (LSP GoTo)" },
-            { "<leader>t", group = "Test Runner" },
-            { "<leader>w", group = "Window Management" },
-        })
-    end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"echasnovski/mini.icons",
+	},
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		-- {
+		--   "<leader>?",
+		--   function()
+		--     require("which-key").show({ global = false })
+		--   end,
+		--   desc = "Buffer Local Keymaps (which-key)",
+		-- },
+	},
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>c", group = "Code (Diagnostics)" },
+			{ "<leader>d", group = "Debugger (DAP)" },
+			{ "<leader>g", group = "Code Navigation (LSP GoTo)" },
+			{ "<leader>h", group = "Harpoon (File Marks)" },
+			{ "<leader>t", group = "Test Runner" },
+			{ "<leader>w", group = "Window Management" },
+		})
+
+        require('mini.icons').setup()
+	end,
 }
