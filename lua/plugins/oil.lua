07aa0a9fff55f75376b8416f80c06430483f5bf6
ndys <hendy.sg02@gmail.com>
2025-04-03 15:56:58 +0700

    fix: add dependencies in oil plugin

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,12 +4,18 @@ return {
 	---@type oil.SetupOpts
 	opts = {},
 	-- Optional dependencies
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	dependencies = { 
+        { "echasnovski/mini.icons", opts = {} },
+        { "nvim-tree/nvim-web-devicons"},
+    },
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+            view_options = {
+                show_hidden = true,
+            }
+        })
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
